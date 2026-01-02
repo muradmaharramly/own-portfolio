b2b330515c48af98a8dbb9c9c51d437af4f45e87
muradmaharramly <mooradmaharramly@gmail.com>
2026-01-03 00:14:16 +0400

    little edit to pattern and vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom', '@reduxjs/toolkit', 'react-redux'],
+          ui: ['framer-motion', 'react-icons', 'react-toastify', 'qrcode.react'],
+          supabase: ['@supabase/supabase-js']
+        }
+      }
+    }
   }
 })
